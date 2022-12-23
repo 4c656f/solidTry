4c1559653cb19c8147a9a4c31fea1576af9614bc
4c656f <vlevo.yovlyo@yandex.ru>
2022-12-23 18:37:14 +0600

    add image

---
+++ src/components/ui/Image/CustomImage.tsx
@@ -0,0 +1,62 @@
+import {Component, createSignal, onMount} from "solid-js";
+import classes from './Image.module.scss';
+
+type ImageProps = {
+    src: string;
+    width: number;
+    height: number;
+    alt: string;
+    className?: string
+    quality?: number
+}
+
+const CustomImage:Component<ImageProps> = (props:ImageProps) => {
+
+
+    const {
+        src,
+        width,
+        alt,
+        height,
+        className,
+        quality
+    } = props
+
+
+    const [isLoaded, setIsLoaded] = createSignal(false)
+
+    onMount(()=>{
+        const img = new Image(width, height)
+        img.src = src
+        console.log('loadStart')
+        img.onload = ()=> {
+            console.log('loaded')
+            setIsLoaded(true)
+        }
+    })
+
+
+
+    return (
+        <
+            // class={classes.container}
+        >
+            {!isLoaded() ?
+                <div
+                    class={classes.placeholder}
+                />: <img
+                    src={src}
+                    alt={alt}
+                    height={height}
+                    width={width}
+                    class={`${className ? className : ""} ${classes.image} `}
+                    // style={{objectFit: 'cover'}}
+
+                />
+            }
+
+        </>
+    );
+};
+
+export default CustomImage;
