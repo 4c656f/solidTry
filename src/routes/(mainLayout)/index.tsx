5f85b112e9497c0a4775ca27d53088484d36e3f6
4c656f <vlevo.yovlyo@yandex.ru>
2023-03-12 18:38:31 +0600

    optimise import

--- src/routes/(mainLayout)/index.tsx
+++ src/routes/(mainLayout)/index.tsx
@@ -1,11 +1,4 @@
-import {Component, createEffect, createSignal, For, Show} from "solid-js";
-import {createServerData$} from "solid-start/server";
-import {prismaClient} from "~/db";
-import classes from './index.module.scss'
-import {unwrap} from "solid-js/store";
-import {getPosts, IGetPosts} from "~/common/prisma/rawQueries";
-import {getUserFromSession} from "~/shared/helpers/session";
-import PostSmall from "~/entities/PostSmall/PostSmall";
+import {Component} from "solid-js";
 import {IndexPageLayout} from "~/pages/IndexPage/ui/IndexPageLayout";
 
 
